d38e32c8235197455154048eb503633e8ef3b46a
Marisol Lopez <lmarisolopez@gmail.com>
2017-07-23 15:28:34 -0700

    routes for runs controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post 'runs', to: 'runs#create'
+  # patch 'runs', to: 'runs#update'
+  post 'runs/notify' => 'runs#notify'
+  post 'runs/end_run', to: 'runs#end_run'
+  # resources :run_not_ended
+  post 'runs/run_not_ended', to: 'runs#run_not_ended'
+  # post 'runnotifier/run_not_ended', to: 'runnotifier/run_not_ended'
+
+
+
 end
